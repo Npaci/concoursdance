e0e0116091f7ee4e78fffa726e552a7dea62ec51
pngabo <tipspaci@gmail.com>
2021-09-27 16:17:22 +0200

    Fisrt commit

---
+++ src/main/java/com/sampac/concoursdance/ConcoursdanceApplication.java
@@ -0,0 +1,13 @@
+package com.sampac.concoursdance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursdanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcoursdanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampac/concoursdance/ConcoursdanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampac.concoursdance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcoursdanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
