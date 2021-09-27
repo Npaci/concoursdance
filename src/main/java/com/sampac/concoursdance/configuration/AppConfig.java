c79cc2ac5515e183a68ac4d93210fca5de5295e8
SamiMas24 <samirahmasimina@gmail.com>
2021-09-27 20:18:17 +0200

    modif menu

--- src/main/java/com/sampac/concoursdance/configuration/AppConfig.java
+++ src/main/java/com/sampac/concoursdance/configuration/AppConfig.java
@@ -1,7 +1,16 @@
 package com.sampac.concoursdance.configuration;
 
+import com.sampac.concoursdance.presentation.Menu;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public Scanner scanner(){
+        System.out.println("instanciation de scanner");
+        return new Scanner(System.in);
+    }
 }
