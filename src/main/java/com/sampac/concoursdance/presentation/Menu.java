43269502dbc9b4bcd90ecfcf3d2de8c4e08d6926
SamiMas24 <samirahmasimina@gmail.com>
2021-09-28 11:03:18 +0200

    modif detail concours

--- src/main/java/com/sampac/concoursdance/presentation/Menu.java
+++ src/main/java/com/sampac/concoursdance/presentation/Menu.java
@@ -1,5 +1,9 @@
 package com.sampac.concoursdance.presentation;
 
+import com.sampac.concoursdance.exceptions.ElementNotFoundException;
+import com.sampac.concoursdance.metier.dto.CandidatDTO;
+import com.sampac.concoursdance.metier.dto.ConcoursDTO;
+import com.sampac.concoursdance.metier.dto.JuryDTO;
 import com.sampac.concoursdance.metier.services.CandidatServiceImpl;
 import com.sampac.concoursdance.metier.services.ConcoursServiceImpl;
 import com.sampac.concoursdance.metier.services.JuryServiceImpl;
@@ -48,13 +52,43 @@ public class Menu {
         switch (choix){
             case 1 -> quit();
             case 2 -> displayAll();
-            case 3 -> quit();
+            case 3 -> detailConcours();
             case 4 -> quit();
             case 5 -> quit();
             case 6 -> quit();
             default -> System.out.println("choix invalide");
         }
     }
+
+    private void detailConcours() {
+        System.out.println(Color.ANSI_GREEN.val+"veuiller donner id du concours: ".toUpperCase()+Color.ANSI_RESET.val);
+        long id = Long.parseLong(scan.nextLine());
+        try {
+            ConcoursDTO concoursDTO=con_service.getByID(id);
+            System.out.println(Color.ANSI_CYAN.val+"Détail du concours de "+concoursDTO.getTheme().toUpperCase());
+            System.out.println("Date du concours: "+concoursDTO.getDate());
+            System.out.println("Information: "+concoursDTO.getDescription()+Color.ANSI_RESET.val);
+
+            System.out.println(Color.ANSI_PURPLE.val+"Liste des juries".toUpperCase()+Color.ANSI_RESET.val);
+            for (JuryDTO juryDTO : concoursDTO.getJuges()) {
+                System.out.format("| %d | %s | %s |\n",
+                        juryDTO.getId(),
+                        juryDTO.getNom(),
+                        juryDTO.getExpertise());
+            }
+            System.out.println(Color.ANSI_BLUE.val+"Liste des Participant ".toUpperCase()+Color.ANSI_RESET.val);
+            for (CandidatDTO candidatDTO : concoursDTO.getParticipants()) {
+                System.out.format("| %d | %s | %d ans |\n",
+                        candidatDTO.getId(),
+                        candidatDTO.getNom(),
+                        candidatDTO.getAge());
+            }
+
+        } catch ( ElementNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     private void displayAll(){
         con_service.getAll()
                 .forEach( System.out::println );
